e0aae726d9d565cefa602eee09441c0030b655b9
Ezzeri Esa <ezzeri.esa@gmail.com>
2023-01-06 16:37:41 -0800

    exercises - 1.1 minimal key-value store

--- src/kvserver.py
+++ src/kvserver.py
@@ -5,23 +5,57 @@ class KVApplication:
     def __init__(self):
         self.data = {}
 
-    def get(key):
-        return data.get(key)
+    def has(self, key):
+        return key in self.data
 
-    def set(key, value):
-        data[key] = value
+    def get(self, key):
+        return self.data.get(key)
 
-    def delete(key):
-        if key in data:
-            del data[key]
+    def set(self, key, value):
+        self.data[key] = value
+
+    def delete(self, key):
+        if key in self.data:
+            del self.data[key]
 
 
 class KVServer:
     def __init__(self):
+        self.app = KVApplication()
+
+        # TODO: Carve out in separate method.
         self.sock = socket(AF_INET, SOCK_STREAM)
         self.sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, True)
+        # TODO: Create config.
         self.sock.bind(("localhost", 8000))
         self.sock.listen()
+
+    def handle(self, request):
+        command, *arguments = request.split(" ")
+
+        # TODO: Check request validity and return codes in separate method.
+        if command == "set":
+            assert len(arguments) == 2
+            self.app.set(arguments[0], arguments[1])
+            return True, None
+
+        # TODO: Consider removing existence check to simplify interface.
+        elif command == "get":
+            assert len(arguments) == 1
+
+            if self.app.has(arguments[0]):
+                return True, self.app.get(arguments[0])
+
+            return False, None
+
+        elif command == "delete":
+            assert len(arguments) == 1
+
+            if self.app.has(arguments[0]):
+                self.app.delete(arguments[0])
+                return True, None
+
+            return False, None
 
     def run(self):
         while True:
@@ -30,11 +64,21 @@ class KVServer:
 
             try:
                 while True:
-                    request = client.recv(10)
+                    # TODO: Log requests and response.
+                    request = client.recv(32)
 
                     if request != b"":
-                        print("request", request.decode("ascii"))
-                        client.sendall(request)
+                        # TODO: Use bencode for request and response.
+                        is_valid, result = self.handle(request.decode("ascii"))
+
+                        if result is not None:
+                            response = result
+                        else:
+                            response = "ok" if is_valid else "invalid"
+
+                        print(request.decode("ascii"), response)
+
+                        client.sendall(response.encode("ascii"))
 
             except IOError:
                 client.close()
