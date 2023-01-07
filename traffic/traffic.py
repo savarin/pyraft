4f394a9eb5fef931e3c0dfd7efd42783f361524e
Ezzeri Esa <ezzeri.esa@gmail.com>
2023-01-07 15:19:40 -0800

    exercises - 1.2 update docstrings

--- traffic/traffic.py
+++ traffic/traffic.py
@@ -51,7 +51,6 @@ class TrafficLight:
         state 3 - 5 seconds
             EW R
             NS Y
-        back to state 0
 
     v2
     - how to deal with timer set for 30 seconds and button pressed? set up
@@ -61,10 +60,28 @@ class TrafficLight:
     v3
     - implement 15 second check
 
+        state 0 - 15 seconds, NS button only changes flag
+            EW G
+            NS R
+        state 1 - 15 seconds, NS button switches state
+            EW G
+            NS R
+        state 2 - 5 seconds
+            EW Y
+            NS R
+        state 3 - 15 seconds, EW button only changes flag
+            EW R
+            NS G
+        state 4 - 45 seconds, EW button switches state
+            EW R
+            NS G
+        state 4 - 5 seconds
+            EW R
+            NS Y
+
     v4
     - how to enable better testing? carve out socket-related functionality into
       separate class. handle based on output.
-
     """
 
     def __post_init__(self):
@@ -85,36 +102,35 @@ class TrafficLight:
 
     def handle_clock_tick(self):
         # If button pressed in first 15 seconds, then toggle on next tick.
-        if self.button:
-            if self.counter % 6 in {0, 3}:
-                self.update()
+        if self.button and self.get_state() in {0, 3}:
+            self.update()
 
         self.update()
         return self.counter
 
-    def handle_ns_button(self):
-        # Change state only when in state 1.
-        if self.counter % 6 == 1:
-            self.update()
-            return self.counter
-
+    def handle_north_south_button(self):
         # Set button to true only when in state 0.
-        if self.counter % 6 == 0:
+        if self.get_state() == 0:
             self.button = True
             return -2
+
+        # Change state only when in state 1.
+        elif self.get_state() == 1:
+            self.update()
+            return self.counter
 
         return -1
 
-    def handle_ew_button(self):
-        # Change state only when in state 4.
-        if self.counter % 6 == 4:
-            self.update()
-            return self.counter
-
+    def handle_east_west_button(self):
         # Set button to true only when in state 3.
-        if self.counter % 6 == 3:
+        if self.get_state() == 3:
             self.button = True
             return -2
+
+        # Change state only when in state 4.
+        elif self.get_state() == 4:
+            self.update()
+            return self.counter
 
         return -1
 
@@ -127,11 +143,10 @@ class TrafficLight:
         self.counter += 1
 
     def update_lights(self):
-        state = self.counter % 6
-        ew_color, ns_color = colors_by_state[state]
+        east_west_color, north_south_color = colors_by_state[self.get_state()]
 
-        self.send_message("EW", ew_color)
-        self.send_message("NS", ns_color)
+        self.send_message("EW", east_west_color)
+        self.send_message("NS", north_south_color)
 
     def reset_button(self):
         self.button = False
@@ -142,63 +157,62 @@ class TrafficLight:
         )
 
 
-def convert_counter(counter):
-    previous_state = (counter - 1) % 6
-    current_state = counter % 6
-    sleep_time = sleep_time_by_state[current_state]
-    status_update = f"previous: {previous_state}, current: {current_state} for {sleep_time} seconds..."
+def convert_state(state):
+    sleep_time = sleep_time_by_state[state]
+    status_update = f"switch to {str(state)} for {sleep_time} seconds..."
 
     return sleep_time, status_update
 
 
 if __name__ == "__main__":
-    # TODO: Refactor so that event queue handles dispatch, light handles state.
-    light = TrafficLight()
+    # TODO: Refactor so that queue handles dispatch, light handles state.
+    traffic_light = TrafficLight()
     event_queue = queue.Queue()
 
     def enqueue(message):
-        print(f"enqueue {message}, counter {light.get_counter()}")
-        event_queue.put((message, light.get_counter()))
+        print(f"enqueue {message} with counter {traffic_light.get_counter()}")
+        event_queue.put((message, traffic_light.get_counter()))
 
     def listen():
         while True:
-            message, _ = light.receive_message()
+            message, _ = traffic_light.receive_message()
             enqueue(message.decode("ascii"))
 
-    def sleep(interval, message):
+    def sleep(interval):
         time.sleep(interval)
-        enqueue(message)
+        enqueue("tick")
 
     def status():
-        sleep_time, status_update = convert_counter(light.counter)
+        sleep_time, status_update = convert_state(traffic_light.get_state())
         print(status_update)
 
-        threading.Thread(target=sleep, args=(sleep_time, "tick")).start()
+        threading.Thread(target=sleep, args=(sleep_time,)).start()
 
     threading.Thread(target=listen, args=()).start()
-    threading.Thread(target=sleep, args=(1, "tick")).start()
+    threading.Thread(target=sleep, args=(1,)).start()
 
     while True:
         event, event_counter = event_queue.get()
 
         # Ignore timer event if have moved on from timer.
-        if event_counter < light.get_counter():
+        if event_counter < traffic_light.get_counter():
             print("ignore stale event.")
             continue
 
         match event:
             case "tick":
-                light_counter = light.handle_clock_tick()
+                light_counter = traffic_light.handle_clock_tick()
 
             case "NS":
-                light_counter = light.handle_ns_button()
+                light_counter = traffic_light.handle_north_south_button()
 
             case "EW":
-                light_counter = light.handle_ew_button()
+                light_counter = traffic_light.handle_east_west_button()
 
         if light_counter == -1:
             print(f"ignore {event} button.")
             continue
+
         elif light_counter == -2:
             print(f"button state switched to true.")
             continue
