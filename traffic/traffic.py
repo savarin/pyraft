73e6623e60d88e7528b6fba7dc79dd84ed0a0e92
Ezzeri Esa <ezzeri.esa@gmail.com>
2023-01-07 05:28:13 -0800

    exercises - 1.2 implement handle clock tick

--- traffic/traffic.py
+++ traffic/traffic.py
@@ -3,35 +3,64 @@
 # Implement traffic light control software.  
 #
 # Challenge:  Make something that can be tested/debugged. 
+from dataclasses import dataclass
+from socket import socket, AF_INET, SOCK_DGRAM
 
 
+colors_by_state = {
+    0: ("G", "R"),
+    1: ("Y", "R"),
+    2: ("R", "G"),
+    3: ("R", "Y"),
+}
+
+port_by_direction = {
+    "ew": 8000,
+    "ns": 9000,
+}
+
+@dataclass
 class TrafficLight:
     """
     v1 - simply change lights according to timer.
 
-        state 0 - 5 seconds
-            EW R
-            NS R
-        state 1 - 30 seconds
+        state 0 - 30 seconds
             EW G
             NS R
-        state 2 - 5 seconds
+        state 1 - 5 seconds
             EW Y
             NS R
-        state 3 - 60 seconds
+        state 2 - 60 seconds
             EW R
             NS G
-        state 4 - 5 seconds
+        state 3 - 5 seconds
             EW R
             NS Y
-        back to state 1
+        back to state 0
     """
+    def __post_init__(self):
+        self.socket = socket(AF_INET, SOCK_DGRAM)
+        self.state = 0
 
     def handle_clock_tick(self):
-        ...
+        self.state = (self.state + 1) % 4
+        self.update_lights()
 
     def handle_ew_button(self):
         ...
 
     def handle_ns_button(self):
         ...
+
+    def update_lights(self):
+        ew_color, ns_color = colors_by_state[self.state]
+
+        self.send_message("ew", ew_color)
+        self.send_message("ns", ns_color)
+
+    def send_message(self, direction, message):
+        self.socket.sendto(message.encode("ascii"), ("localhost", port_by_direction[direction]))
+
+
+if __name__ == "__main__":
+    light = TrafficLight()
